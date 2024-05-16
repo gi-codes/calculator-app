1c66a3af21fd539597adba1d2196d7ecb2c4e094
hanan <hanankahiye@gmail.com>
2024-05-16 17:36:14 +0100

    value error

--- be/calculator.py
+++ be/calculator.py
@@ -5,11 +5,11 @@
 def calculator(x, y, operation):
   answer = 0
   
-  user_x = int(input("choose x "))
-  user_y = int(input("choose y "))
+  # user_x = int(input("choose x "))
+  # user_y = int(input("choose y "))
 
-  print(f"the user chose  {user_x}")
-  print(f"the user chose  {user_y}")
+  # print(f"the user chose  {user_x}")
+  # print(f"the user chose  {user_y}")
 
   # result = user_x + user_y
   # print(f"the result will be {result}")
@@ -21,9 +21,9 @@ def calculator(x, y, operation):
   elif(operation== "*"):
     answer = x * y
   elif(operation== "/"):
-  #   if y == 0:
-  #     raise ValueError("Cannot divide by zero")
-  #   else:
+    if y == 0:
+      raise ValueError("Cannot divide by zero")
+    else:
       answer = x / y
   else:
     print("Please provide a math operation")
