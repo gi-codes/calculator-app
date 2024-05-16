95de4c8ac3af23039fc3bbaf29933ed715dbb94e
hanan <hanankahiye@gmail.com>
2024-05-16 17:23:08 +0100

    added value error

--- be/calculator.py
+++ be/calculator.py
@@ -11,22 +11,21 @@ def calculator(x, y, operation):
   print(f"the user chose  {user_x}")
   print(f"the user chose  {user_y}")
 
-  result = user_x - user_y
-  print(f"the result will be {result}")
+  # result = user_x + user_y
+  # print(f"the result will be {result}")
 
-#   if(operation == "+"):
-#     answer = x + y
-#   elif(operation == "-"):
-#     answer = x - y
-#   elif(operation== "*"):
-#     answer = x * y
-#   elif(operation== "/"):
-#     answer = x / y
-#   return answer
-
-
-
-
-result = calculator(10, 5, "+")
-# print(f"the result is: {result}")
-#add user 
+  if(operation == "+"):
+    answer = x + y
+  elif(operation == "-"):
+    answer = x - y
+  elif(operation== "*"):
+    answer = x * y
+  elif(operation== "/"):
+  #   if y == 0:
+  #     raise ValueError("Cannot divide by zero")
+  #   else:
+      answer = x / y
+  else:
+    print("Please provide a math operation")
+  
+  return answer
