d1715722c7cf706ae35425cc4cc9b80f1d76800a
hanan <hanankahiye@gmail.com>
2024-05-22 12:21:20 +0100

    changed server.py and js

--- be/server.py
+++ be/server.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello world'
+    return 'calculator app'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
--- fe/index.js
+++ fe/index.js
@@ -1,3 +1,5 @@
 const buttonClick = () => {
   console.log("button was clicked!");
 };
+const result = fetch("http://localhost:5000");
+console.log(result);
